a031a3d5ce782cd89210e2e385aaa46c262eded4
CodingNinja42 <rohitahire545@gmail.com>
2025-01-27 15:57:45 +0530

    first commit

---
+++ Models/employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace FileUpload.Models
+{
+    public class employee
+    {
+        [Key]
+        public int eid { get; set; }
+        public string ename { get; set; }
+        public string email { get; set; }
+        public string epass { get; set; }
+        public string ecpass { get; set; }
+        public string eimage { get; set; }
+        [NotMapped]
+        public HttpPostedFileBase orimage { get; set; }
+    }
+}
---
+++ Models/employeeContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace FileUpload.Models
+{
+    public class employeeContext : DbContext
+    {
+        public DbSet<employee> employees { get; set; }
+    }
+}
